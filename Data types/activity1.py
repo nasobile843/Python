ad745a3611a0649454c42f59e680b329cce8e867
nasobile843 <nasobilegwakisa@gmail.com>
2024-09-13 16:40:29 +0300

    String operations

---
+++ Data types/activity1.py
@@ -0,0 +1,18 @@
+name = "penguin"
+age = 15
+is_student = True
+weight = 38.5
+print("Name :", name)
+print("Data Type of name is", type(name))
+print("age :", age)
+print("Data Type of age is :", type(age))
+print("is_student :", is_student)
+print("Weight :", weight)
+print("Data Type of weight is :", type(weight))
+print("\n After Type Casting. . . .")
+age = str(age)
+print(age)
+print("Data TYpe of age is :", type(age))
+weight = int(weight)
+print(weight)
+print("Data Type of weight is :", type(weight))
