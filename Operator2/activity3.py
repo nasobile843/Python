78669bee9ffab5a3eeae51ace6a634e2c8efd4b5
nasobile843 <nasobilegwakisa@gmail.com>
2024-09-22 14:22:57 +0300

    Operators2

---
+++ Operator2/activity3.py
@@ -0,0 +1,21 @@
+a = 10
+b = 12
+c = 12
+
+print(a != b)
+print(b != c)
+
+
+a = "python"
+b = "coding"
+
+if a != b:
+    print(a, 'and' ,b, 'are different.')
+
+
+
+a =  int (input("enter a number"))
+
+if a%2 != 0 :
+    print(a, "is not an even number.")
+    
