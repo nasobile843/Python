8c1756fc9a1e260bbde28378ef197d5b5996abad
nasobile843 <nasobilegwakisa@gmail.com>
2024-09-27 16:39:18 +0300

    nested conditional statments

---
+++ Nested  Conditional Statment/activity1.py
@@ -0,0 +1,15 @@
+medical_cause=input("did you have a medical cause Y or N: ")
+
+atten = int(input("entar attenence of student: "))
+
+
+
+
+if medical_cause == 'Y':
+    print("You are allowed")
+else:
+    if atten>=75:
+        print("Allowed")
+    else:
+        print("Not Allowed")
+        
---
+++ homework/homework1.py
@@ -0,0 +1,2 @@
+a = int(input("entar a character"))
+
