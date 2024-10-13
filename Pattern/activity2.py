c18243d9f8de69702cf29d1b80b1ca1275448380
nasobile843 <nasobilegwakisa@gmail.com>
2024-10-13 14:22:43 +0300

    Turtle library

--- Pattern/activity2.py
+++ Pattern/activity2.py
@@ -2,7 +2,7 @@ print ("Half Pyramid Pattern of Numbers(i): ")
 n = int(input("Enter number of rows: "))
 t = 1
 for i in range(n):
-    for j in range(i+1):
+    for j in range(1+i):
        print(t, end="")
-       t = t+1
+       t = 1+t
     print()  
