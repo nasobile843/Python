0843dc488bebd9624cfa8129599e70ad5a390131
nasobile843 <nasobilegwakisa@gmail.com>
2024-10-13 14:44:05 +0300

    After class

--- homework/homework1.py
+++ homework/homework1.py
@@ -1,2 +1,16 @@
-a = int(input("entar a character"))
+import turtle
+ttl = turtle.Turtle()
+ttl.forward(150)
+ttl.left(90)
+
+ttl.forward(150)
+ttl.left(90)
+
+ttl.forward(150)
+ttl.left(90)
+
+ttl.forward(150)
+ttl.left(90)
+
+turtle.done()
 
