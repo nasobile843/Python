3535ae733562dc90137a358e4212a0b7cce7ac8c
nasobile843 <nasobilegwakisa@gmail.com>
2024-10-18 16:39:45 +0300

    Function

---
+++ Function/activity2.py
@@ -0,0 +1,10 @@
+def weather_condition():
+    print('The weather is pleasent in:', spring)
+    print('The weather is the same in:', autumn)
+
+
+spring = "autumn"
+autumn = "spring"
+
+weather_condition()
+
