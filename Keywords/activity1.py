e235bf799fb4a2efe8e209273534f7a501447820
nasobile843 <nasobilegwakisa@gmail.com>
2024-10-25 17:41:31 +0300

    keywords

---
+++ Keywords/activity1.py
@@ -0,0 +1,7 @@
+def func(a,b):
+    return(a+b)
+
+
+sum = func(5,5)
+print(sum)
+
---
+++ Keywords/activity3.py
@@ -0,0 +1,7 @@
+var = 10
+while var > 10:
+    var = var - 1
+    if var == 5:
+        continue
+    print('/n Current variable value:', var)
+print("/nGood bye !")
