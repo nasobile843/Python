453e49a0c550d82141918c37bb9f80117ee47ae1
nasobile843 <nasobilegwakisa@gmail.com>
2024-11-15 17:42:11 +0300

    All About Dictionaries

---
+++ All About Dictionaries/activity3.py
@@ -0,0 +1,6 @@
+SPCMcountry_code = {'India' : '0091', 'Australia' :'0025', 'Nepal' : '00977'}
+
+print("Country code for India -")
+print(country_code.get('India' : 'Note Found'))
+
+print("Country ")
