351c43595b7c0785c2a39340a9db4c62d3f06fe3
nasobile843 <nasobilegwakisa@gmail.com>
2024-12-06 17:43:39 +0300

    Inheritance

---
+++ Inheritance/activity1.py
@@ -0,0 +1,29 @@
+class dad:
+
+    def __init__(self, eyes, aggressive):
+        self.eyes = eyes
+        self.aggressive = aggressive
+
+    def display(self):
+        print("Your eye color is", self.eyes)
+        print("You are aggessive", self.aggressive)
+
+class son(dad):
+
+    def __init__(self, name, age, eyes, aggressive):
+        self.name = name
+        self.age = age
+
+
+        dad.__init__(self, eyes, aggressive)
+
+
+obj = son("Penguine", 8, "blue", True)
+
+obj.display()
+
+
+
+         
+        
+
---
+++ Inheritance/activity3.py
@@ -0,0 +1,9 @@
+class Bird:
+
+    def __init__(self):
+        print("Bird is ready")
+
+    def whoisThis(self):
+        print(Bird)
+
+    
