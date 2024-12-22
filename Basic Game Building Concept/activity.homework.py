4ec87d3f95dbc578eafa09d7c3da09ff136ac74e
nasobile843 <nasobilegwakisa@gmail.com>
2024-12-22 14:34:17 +0300

    homework

---
+++ Basic Game Building Concept/activity.homework.py
@@ -0,0 +1,28 @@
+import pygame
+
+pygame.init()
+
+window = pygame.display.set_mode((400, 400))
+window.fill((255, 255, 255))
+GREEN = (0, 255, 0)
+pygame.draw.circle(window, GREEN, (300, 300), 50)
+pygame.draw.circle(window, GREEN, (100, 100), 50, 3)
+pygame.display.update()
+running = True
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+screen = pygame.display.set_mode((400, 300))
+done = False
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+    pygame.draw.rect(screen, (0, 125, 255), pygame.Rect(30, 30, 60, 60))
+
+    
+    
